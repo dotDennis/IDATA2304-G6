e8f0e787304a965df5d7e7bd97bfe62c548c139a
Dennis <71315485+dotDennis@users.noreply.github.com>
2025-11-10 12:58:28 +0100

    Added enums for bare minimum NodeTypes, improves scalability

---
+++ src/main/java/group6/entity/NodeType.java
@@ -0,0 +1,9 @@
+package group6.entity;
+
+/**
+ * NodeType represents the type of a Node in the system.
+ */
+public enum NodeType {
+  SENSOR,   // physical device to me sensor
+  CONTROL, // farmers controlpanel
+}
