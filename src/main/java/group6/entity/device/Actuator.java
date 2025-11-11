aac170cbda2feb31f0ea0e360186712e192f53f3
Dennis <71315485+dotDennis@users.noreply.github.com>
2025-11-11 13:40:43 +0100

    Implement Actuator class with constructors and state management, extending from Device.

--- src/main/java/group6/entity/device/Actuator.java
+++ src/main/java/group6/entity/device/Actuator.java
@@ -1,5 +1,45 @@
 package group6.entity.device;
 
-public class Actuator {
-    
+/**
+ * SensorNode represents a node that contains sensors/actuators in the system.
+ * It extends the abstract Node class.
+ * 
+ * @author dotDennis
+ * @since 0.1.0
+ */
+public class Actuator extends Device<ActuatorType> {
+
+    private boolean state;
+
+    /**
+     * Constructs an Actuator with the specified ID and type.
+     * 
+     * @param deviceId   unique identifier for the actuator
+     * @param deviceType type of the actuator
+     * @throws IllegalArgumentException if deviceId is null/blank or deviceType is
+     *                                  null
+     */
+    public Actuator(String deviceId, ActuatorType deviceType) {
+        super(deviceId, deviceType);
+    }
+
+    /**
+     * Constructs an Actuator with the specified ID, type, and initial state.
+     * 
+     * @param deviceId   unique identifier for the actuator
+     * @param deviceType type of the actuator
+     * @param state      initial state of the actuator (true for ON, false for OFF)
+     * @throws IllegalArgumentException if deviceId is null/blank or deviceType is
+     *                                  null
+     */
+    public Actuator(String deviceId, ActuatorType deviceType, boolean state) {
+        super(deviceId, deviceType);
+        this.state = state;
+    }
+
+    // ---------- Getters ----------
+    public boolean getState() {
+        return state;
+    }
+
 }
