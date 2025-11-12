7fb114a5f43d1d6b8c6c0aa0331a844d725ee7a4
Dennis <71315485+dotDennis@users.noreply.github.com>
2025-11-12 23:05:08 +0100

    Made MessageType be enum

--- src/main/java/group6/protocol/MessageType.java
+++ src/main/java/group6/protocol/MessageType.java
@@ -1,21 +1,17 @@
 package group6.protocol;
 
 /**
- * Constants for protocol message
- *
- * @author Fidjor
+ * Protocol message types.
+ * @author Fidjor, dotDennis
  * @since 0.1.0
  */
-
-public class MessageType {
-
-  public static final String SENSOR_DATA = "SENSOR_DATA";
-  public static final String ACTUATOR_STATUS = "ACTUATOR_STATUS";
-  public static final String COMMAND = "COMMAND";
-  public static final String ACK = "ACK";
-  public static final String ERROR = "ERROR";
-
-  private MessageType() {
-  }
-
+public enum MessageType {
+    HELLO,
+    WELCOME,
+    DATA,
+    COMMAND,
+    SUCCESS,
+    FAILURE,
+    KEEPALIVE,
+    ERROR
 }
