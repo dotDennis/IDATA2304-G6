65ea580f1ff7dcdb9d615a5dfacec4c4cc9a7278
Dennis <71315485+dotDennis@users.noreply.github.com>
2025-11-20 17:05:38 +0100

    moved files, fixed imports

--- src/main/java/group6/net/TcpServer.java
+++ src/main/java/group6/net/TcpServer.java
@@ -7,8 +7,8 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import group6.entity.node.ClientHandler;
 import group6.entity.node.SensorNode;
+import group6.net.server.ClientHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
