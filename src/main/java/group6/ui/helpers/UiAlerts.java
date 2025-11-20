da0e6e7a4409b976827d731eaebb093e7f00c94c
Dennis <71315485+dotDennis@users.noreply.github.com>
2025-11-20 17:09:31 +0100

    Added docs, removed unused verisonutil

--- src/main/java/group6/ui/helpers/UiAlerts.java
+++ src/main/java/group6/ui/helpers/UiAlerts.java
@@ -7,17 +7,39 @@ import javafx.scene.control.Alert;
  */
 public final class UiAlerts {
 
+  /**
+   * Private constructor to prevent instantiation.
+   */
   private UiAlerts() {
   }
 
+  /**
+   * Shows an information alert.
+   * 
+   * @param title   the title of the alert
+   * @param message the message of the alert
+   */
   public static void info(String title, String message) {
     show(title, message, Alert.AlertType.INFORMATION);
   }
 
+  /**
+   * Shows an error alert.
+   * 
+   * @param title   the title of the alert
+   * @param message the message of the alert
+   */
   public static void error(String title, String message) {
     show(title, message, Alert.AlertType.ERROR);
   }
 
+  /**
+   * Private helper to show an alert.
+   * 
+   * @param title   the title
+   * @param message the message
+   * @param type    the alert type
+   */
   private static void show(String title, String message, Alert.AlertType type) {
     Alert alert = new Alert(type);
     alert.setTitle(title);
